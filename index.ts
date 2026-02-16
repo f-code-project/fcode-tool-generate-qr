a55126ac9ed4020cd136000177995a8ce73709da
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-02-16 19:12:01 +0700

    feat: done all

--- index.ts
+++ index.ts
@@ -1,19 +1,32 @@
 import { downloadQR, encodeBase64, generateQR } from "./helper";
-const file = Bun.file("data.txt");
-const exportFile = Bun.file("export.txt");
-const writer = exportFile.writer();
-const data = await file.text();
-for (let line of data.split("\n")) {
+const fileReader = Bun.file("data.txt");
+const data = (await fileReader.text()).split("\n");
+
+const fileExporter = Bun.file("export.txt");
+
+const writer = fileExporter.writer();
+
+// current api
+const promiseUrls: Promise<string>[] = [];
+for (let line of data) {
   const [studentCode, fullName] = line.trim().split("|");
   const token = encodeBase64(`${fullName}|${studentCode}`);
-  const data = await generateQR(token);
-  console.log(data);
-
-  const byteWriten = await downloadQR(`https:${data}`, `${studentCode}.png`);
-
+  const callAPIGenerate = generateQR(token);
+  promiseUrls.push(Promise.resolve(callAPIGenerate));
+  //   const byteWriten = await downloadQR(
+  //     `https:${data}`,
+  //     `[${studentCode}] - ${fullName}.png`,
+  //   );
+  //   console.log(`${fullName} - ${studentCode} - ${byteWriten} bytes written`);
+  writer.write(`${fullName}|${token}\n`);
+}
+const urls = await Promise.all(promiseUrls);
+for (let i = 0; i < urls.length; i++) {
+  const [studentCode, fullName] = data[i]!.trim().split("|");
+  const byteWriten = await downloadQR(
+    `https:${urls[i]!}`,
+    `[${studentCode}] - ${fullName}.png`,
+  );
   console.log(`${fullName} - ${studentCode} - ${byteWriten} bytes written`);
-
-  writer.write(`${fullName}|${token}|${data}\n`);
-  break;
 }
 writer.flush();
